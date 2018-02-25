45f192bce1a4da8f9e0af35df4b2afa9a5c9a1ec
Terence Mangram <tmangram@gmail.com>
2018-02-25 22:34:26 +0900

    Add Layout structure and styling

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Aux from "../../hoc/Aux";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+import classes from "./Layout.css";
+
+const layout = (props) => (
+	<Aux>
+		<Toolbar />
+		<main className={classes.Content}>
+			{props.children}
+		</main>
+	</Aux>
+);
+
+// can later declare propTypes maybe
+
+export default layout;
