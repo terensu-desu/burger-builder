ba9bfcbf85e79d747dfb4bae47f911190be3b83c
Terence Mangram <tmangram@gmail.com>
2018-02-25 22:34:42 +0900

    Add Logo component and styling

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Logo from "../../assets/images/burger-logo.png";
+import classes from "./Logo.css";
+
+const logo = (props) => (
+	<div className={classes.Logo}>
+		<img src={Logo} alt="Logo"/>
+	</div>
+);
+
+export default logo;
