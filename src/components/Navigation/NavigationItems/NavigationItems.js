a5f4f99d2f15086d64b78782f8c2d4b0afc6bcb5
Terence Mangram <tmangram@gmail.com>
2018-02-28 00:30:27 +0900

    Add NavLink and active class from react router

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,27 +1,21 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 import classes from "./NavigationItems.css";
 
-const navigationItems = (props) => {
-	const home = true;
-	return (
-		<ul className={classes.NavigationItems}>
-			<li className={classes.NavigationItem}>
-				<a 
-					href="/"
-					className="active">
-						Burger Builder
-					</a>
-			</li>
-			<li className={classes.NavigationItem}>
-				<a 
-					href="/"
-					className={home ? null : "active"}>
-						Checkout
-					</a>
-			</li>
-		</ul>
-	)
-};
+const navigationItems = (props) => (
+	<ul className={classes.NavigationItems}>
+		<li className={classes.NavigationItem}>
+			<NavLink to="/" exact activeClassName={classes.active}>
+				Burger Builder
+			</NavLink>
+		</li>
+		<li className={classes.NavigationItem}>
+			<NavLink to="/orders" activeClassName={classes.active}>
+				Orders
+			</NavLink>
+		</li>
+	</ul>
+)
 
 export default navigationItems;
