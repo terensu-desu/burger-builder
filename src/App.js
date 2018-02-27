c598119a591c2e491094b8951105ab3d357bb3a1
Terence Mangram <tmangram@gmail.com>
2018-02-28 00:27:52 +0900

    Add react-router-dom Router, Switch, Routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Layout from "./containers/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import Checkout from "./containers/Checkout/Checkout";
+import Orders from "./containers/Orders/Orders";
 
 class App extends Component {
   render() {
     return (
-      <Layout>
-        <BurgerBuilder />
-      </Layout>
+      <Router>
+        <Layout>
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/orders" component={Orders} />
+            <Route exact path="/" component={BurgerBuilder}/>
+          </Switch>
+        </Layout>
+      </Router>
     );
   }
 }
