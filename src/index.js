0f2ad6fda1e6a3a07a897bbc3fc91da769728927
Terence Mangram <tmangram@gmail.com>
2018-03-03 10:18:34 +0900

    add process.env check to remove redux dev tools in production

--- src/index.js
+++ src/index.js
@@ -15,8 +15,8 @@ const rootReducer = combineReducers({
 	burgerBuilder: burgerBuilderReducer,
 	order: orderReducer,
 	auth: authReducer
-})
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+});
+const composeEnhancers = process.env.NODE_ENV === "development" ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 const enhancer = composeEnhancers(applyMiddleware(thunk));
 const store = createStore(rootReducer, enhancer);
 
