7edcb9e0f335ddbe423f0c648a3154b38e0c6190
Terence Mangram <tmangram@gmail.com>
2018-03-03 10:15:43 +0900

    Add lazy loading via hoc component for checkout, auth, and orders

--- src/App.js
+++ src/App.js
@@ -4,11 +4,19 @@ import { connect } from "react-redux";
 
 import Layout from "./containers/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
-import Auth from "./containers/Auth/Auth";
 import Logout from "./containers/Auth/Logout/Logout";
-import Checkout from "./containers/Checkout/Checkout";
-import Orders from "./containers/Orders/Orders";
+import asyncComponent from "./hoc/asyncComponent";
 import * as actionCreators from "./store/actions/";
+
+const asyncCheckout = asyncComponent(() => {
+  return import("./containers/Checkout/Checkout")
+});
+const asyncOrders = asyncComponent(() => {
+  return import("./containers/Orders/Orders")
+});
+const asyncAuth = asyncComponent(() => {
+  return import("./containers/Auth/Auth")
+});
 
 class App extends Component {
   componentWillMount() {
@@ -17,7 +25,7 @@ class App extends Component {
   render() {
     let routes = (
       <Switch>
-        <Route path="/auth" component={Auth} />
+        <Route path="/auth" component={asyncAuth} />
         <Route exact path="/" component={BurgerBuilder}/>
         <Redirect to="/" />
       </Switch>
@@ -25,9 +33,10 @@ class App extends Component {
     if(this.props.isAuthenticated) {
       routes = (
         <Switch>
-          <Route path="/checkout" component={Checkout} />
+          <Route path="/checkout" component={asyncCheckout} />
           <Route path="/logout" component={Logout} />
-          <Route path="/orders" component={Orders} />
+          <Route path="/orders" component={asyncOrders} />
+          <Route path="/auth" component={asyncAuth} />
           <Route exact path="/" component={BurgerBuilder}/>
           <Redirect to="/" />
         </Switch>
